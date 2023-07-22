86d2681e9fa71f7eb7f37810e1775b6cea277831
qrai <78605946+quanntdev@users.noreply.github.com>
2023-07-22 23:39:07 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'phone-nestjs-dev',
+            name: 'phone-nestjs-dev-2',
             script: './dist/main.js',
             watch: false,
             env: {
